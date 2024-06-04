314762107b8f512fab1286eb0ba42f7f7d479993
Jared Davidson (Archetapp) <archetapp@gmail.com>
2024-06-04 12:24:37 -0600

    update w/ SwiftUIX

--- Package.swift
+++ Package.swift
@@ -11,11 +11,16 @@ let package = Package(
             name: "DocumentationBuilder",
             targets: ["DocumentationBuilder"]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-docc-plugin.git", from: "1.0.0"),
+        .package(url: "https://github.com/SwiftUIX/SwiftUIX.git", branch: "master"),
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "DocumentationBuilder"),
+            name: "DocumentationBuilder",
+            dependencies: ["SwiftUIX"]),
         .testTarget(
             name: "DocumentationBuilderTests",
             dependencies: ["DocumentationBuilder"]),
