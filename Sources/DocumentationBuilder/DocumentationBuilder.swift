c04539bb3085cfdb8c0ce5afcaea0196108deec1
Jared Davidson (Archetapp) <archetapp@gmail.com>
2024-06-04 12:32:50 -0600

    update

--- Sources/DocumentationBuilder/DocumentationBuilder.swift
+++ Sources/DocumentationBuilder/DocumentationBuilder.swift
@@ -1,2 +1,10 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+
+
+/// Hello world this is a test.
+struct SomethingToDocument {
+    var body: String = ""
+    var title: String = ""
+}
